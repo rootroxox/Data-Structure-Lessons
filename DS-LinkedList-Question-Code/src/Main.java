75fe374b364652364d70eb69f63c324d1d3605fe
Ayberk <56306416+rootroxox@users.noreply.github.com>
2021-08-08 12:17:03 +0300

    LinkedList Example Question

---
+++ DS-LinkedList-Question-Code/src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+
+    /*
+
+        1->2->3->4->5->6->7->8->9->NULL
+
+        1.
+
+        2.
+
+
+
+    */
+
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList();
+        linkedList.append(1);
+        linkedList.append(2);
+        linkedList.append(3);
+        linkedList.append(4);
+        linkedList.append(5);
+        linkedList.append(6);
+        linkedList.append(7);
+        linkedList.append(8);
+        linkedList.append(9);
+
+        linkedList.findMiddle();
+    }
+}
